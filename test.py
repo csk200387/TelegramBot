1eb18fd767f84dd776c5f8d394e265cc41b0ec77
csk200387 <csk200387@gmail.com>
2024-01-01 18:03:43 +0900

    d

--- test.py
+++ test.py
@@ -14,24 +14,24 @@ bot = telegram.Bot(token=TOKEN)
 
 async def main():
     global LAST_ARTICLE_ID
-    while True:
-        articles = get_hotdeal()
-        for article in articles:
-            if article['article_id'] > LAST_ARTICLE_ID:
-                LAST_ARTICLE_ID = article['article_id']
-                title = article['title']
-                price = article['price']
-                category = article['category']
-                store = article['store']
-                link = article['link']
+    try:
+        while True:
+            articles = get_hotdeal()
+            for article in articles:
+                if article['article_id'] > LAST_ARTICLE_ID:
+                    LAST_ARTICLE_ID = article['article_id']
+                    title = article['title']
+                    price = article['price']
+                    category = article['category']
+                    store = article['store']
+                    link = article['link']
 
-                # await bot.send_message(chat_id=CHAT_ID, text=f'[{category}] {title}\n{price}\n{store}\n{link}')
-                await send_hook(title, link, price, category, store, "아카라이브")
-        await asyncio.sleep(60)
+                    # await bot.send_message(chat_id=CHAT_ID, text=f'[{category}] {title}\n{price}\n{store}\n{link}')
+                    await send_hook(title, link, price, category, store, "아카라이브")
+            await asyncio.sleep(60)
+    except Exception as e:
+        bot.send_message(chat_id=CHAT_ID, text=f'에러 발생\n{e}')
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except Exception as e:
-        bot.send_message(chat_id=CHAT_ID, text='프로그램 종료됨\n'+str(e))
+    asyncio.run(main())
